831fa59141998e745fcac7dbf0f43f830029d484
Michał Czyż <michal.czyz35@gmail.com>
2021-03-10 15:53:24 +0100

    ES6 Update (Part 1)

--- main/libraries/main.js
+++ main/libraries/main.js
@@ -1,66 +1,49 @@
-// Menu Functions
-
-$(document).on("click", "#menu-open", function () {
-  $("#menu-bar").animate({left: 0});
-  $("#dimmer").fadeIn();
-});
-$(document).on("click", "#history-open", function () {
-  $("#history-bar").animate({bottom: 0});
-  $("#dimmer3").fadeIn();
-});
-$(document).on("click", "#menu-close", function () {
-  $("#menu-bar").animate({left: "-60%"});
-  $("#dimmer").fadeOut();
-});
-$(document).on("click", "#complex-open", function () {
-  $("#complex").animate({right: 0});
-  $("#dimmer2").fadeIn();
-});
-$(document).on("click", "#complex-close", function () {
-  $("#complex").animate({right: "-80%"}); 
-  $("#dimmer2").fadeOut();
-});
-$(document).on("click", "#dimmer", function () {
-  $("#menu-bar").animate({left: "-60%"});
-  $("#history-bar").animate({bottom: "-=800px"});
-  $("#dimmer").fadeOut();
-});
-$(document).on("click", "#dimmer2", function () {
-  $("#complex").animate({right: "-80%"}); 
-  $("#dimmer2").fadeOut();
-});
-$(document).on("click", "#dimmer3", function () {
-  $("#history-bar").animate({bottom: "-=800px"});
-  $("#dimmer3").fadeOut();
-});
 // Calculator
 
-function Input(val) {
-  var screen = document.getElementById('screen');
-  var preview = document.getElementById('preview');
-  screen.value += val;
+// function Input(val) {
+//   var screen = document.getElementById('screen');
+//   var preview = document.getElementById('preview');
+//   screen.value += val;
+//   preview.value = eval(screen.value);
+// }
+
+const screen = document.getElementById('screen');
+const preview = document.getElementById('preview');
+const clear = document.getElementById('clear');
+const back = document.getElementById('back');
+
+const verify = (input) => {
+  reg = '/([-+]?[0-9]*\.?[0-9]+[\/\+\-\*])+([-+]?[0-9]*\.?[0-9]+)/g';
+  if (input.match(reg)) {
+    return input;
+  }
+  else {
+    return null;
+  }
+}
+
+const input = (value) => {
+  screen.value += value;
+  
+  // if (verify(preview.value) != null) {
+  //   preview.value = eval(screen.value);
+  // }
   preview.value = eval(screen.value);
 }
 
-function Ans() {
-  var screen = document.getElementById('screen');
-  var preview = document.getElementById('preview');
+const answer = () => {
   screen.value = eval(screen.value);
   preview.value = '';
 
-  document.getElementById("clear").style.display = "block";
-  document.getElementById("back").style.display = "none";
+  clear.style.display = "block"; 
+  back.style.display = "none";
 }
 
-function Clear() {
-  var screen = document.getElementById('screen');
+const cls = () => {
   screen.value = "";
 
-  document.getElementById("clear").style.display = "none";
-  document.getElementById("back").style.display = "block";
+  clear.style.display = "none"; 
+  back.style.display = "block";
 }
 
-function Back() {
-  var screen = document.getElementById('screen');
-  screen.value = screen.value.substring(0, screen.value.length - 1);
-}
+const bck = () => screen.value = screen.value.substring(0, screen.value.length - 1);
